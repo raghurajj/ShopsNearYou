09d70e2d1520030fff5786ee3681383d1dd23912
raghurajj <raghurajpratap1999@gmail.com>
2020-06-05 15:15:40 +0530

    modified middleware

--- mysite/settings.py
+++ mysite/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'shops',
     'crispy_forms',
     'whitenoise.runserver_nostatic',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
